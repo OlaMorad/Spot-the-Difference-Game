a651dfad212a351148a96e40b046956dca298e68
Ola Morad <152977933+OlaMorad@users.noreply.github.com>
2025-05-21 00:37:41 +0300

    UI

--- Spot the Difference Game/obj/Debug/net8.0-windows/Spot the Difference Game.AssemblyInfo.cs
+++ Spot the Difference Game/obj/Debug/net8.0-windows/Spot the Difference Game.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Spot the Difference Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4cb0c62729a4cf863959ea088fd69e6b1a3f224")]
 [assembly: System.Reflection.AssemblyProductAttribute("Spot the Difference Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Spot the Difference Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
